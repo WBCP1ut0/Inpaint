cc23a3ed8f286d89da1eab35953daa4119714c78
Muhammad Ahmad <muhammadahmad@Muhammads-MacBook-Air.local>
2025-02-16 02:16:00 +0800

    v0.0.01

---
+++ android/app/src/main/kotlin/org/plutotech/bg/background_remover/MainActivity.kt
@@ -0,0 +1,5 @@
+package org.plutotech.bg.background_remover
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/org/plutotech/bgremover/background_remover/MainActivity.kt
@@ -0,0 +1,5 @@
+package org.plutotech.bgremover.background_remover
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
